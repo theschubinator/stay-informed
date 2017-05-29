14c9aadd1f49e173b9f7fae38b549c0adc5620c9
github username <github email address>
2017-05-29 18:26:23 -0500

    users can create tasks. task#new and task#create actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,21 @@ class TasksController < ApplicationController
 	end
 
 	def new
-		@tast = Task.new
+		@task = Task.new
 	end
 
 	def create
+		user_tasks.build(task_params)
+		current_user.save
+		redirect_to user_tasks_path(current_user)
 	end
+
+	private
+	  def task_params
+	  	params.require(:task).permit(:name, :description, :"due_date(1i)", :"due_date(2i)", :"due_date(3i)", :"due_date(4i)", :"due_date(5i)")
+	  end
+
+	  def user_tasks
+	  	current_user.tasks
+	  end
 end
