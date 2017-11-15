105a93a1315b80f83455451fedbb15527a221b37
github username <github email address>
2017-11-15 15:20:33 -0600

    users show page is now users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,10 @@
 class UsersController < ApplicationController
-	def show
-			if current_user == User.find_by(id: params[:id]) || current_user.admin?
-		  @user = User.find(params[:id])
-		else
-		  flash[:alert] = "You do not have the authorization to view this page."
-		  redirect_to root_path
-		end
-	end
-
-	def profile
-		if current_user == User.find_by(id: params[:user_id]) || current_user.admin?
-		  @user = User.find(params[:user_id])
-		else
-		  flash[:alert] = "You do not have the authorization to view this page."
-		  redirect_to root_path
-		end
-	end
-
 	def update
 		if current_user == User.find_by(id: params[:id]) || current_user.admin?
 		  @user = User.find(params[:id])
 		  @user.role = params[:user][:role].to_i
 		  @user.save
-		  redirect_to user_path(@user)
+		  redirect_to user_profile_path(@user)
 		else
 		  flash[:alert] = "You do not have the authorization to view this page."
 		  redirect_to root_path
