8bac2624f493097270abe6c9b4f782a71e80b661
github username <github email address>
2018-02-19 17:03:44 -0600

    added Procfile for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615190413) do
+ActiveRecord::Schema.define(version: 20171114194822) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170615190413) do
     t.string   "provider"
     t.integer  "uid",                    limit: 8
     t.integer  "role",                             default: 0
+    t.string   "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
